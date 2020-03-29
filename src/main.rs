1055840cbb0f75ab92e46d26b59563a194f22f5f
Haydn Vestal <haydnv@gmail.com>
2020-03-29 15:03:54 -0500

    add HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+mod drive;
+mod http;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Clone, StructOpt)]
+pub struct HostConfig {
+    #[structopt(long = "http_port", default_value = "8702")]
+    pub http_port: u16,
+
+    #[structopt(long = "workspace", default_value = "/tmp/tc")]
+    pub workspace: PathBuf,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let config = HostConfig::from_args();
+
+    println!("Tinychain version {}", VERSION);
+    println!("Working directory: {}", &config.workspace.to_str().unwrap());
+
+    let _workspace = drive::Drive::new(config.workspace);
+    http::listen(config.http_port).await?;
+    Ok(())
 }
