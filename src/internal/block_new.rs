d9cae0743a8ce72b575dd8a609044ee2b0e3b88f
Haydn Vestal <haydnv@gmail.com>
2020-05-29 18:01:50 -0500

    implement block_new::Dir::new_dir

--- src/internal/block_new.rs
+++ src/internal/block_new.rs
@@ -4,7 +4,9 @@ use std::sync::Arc;
 
 use futures::future::BoxFuture;
 
+use crate::error;
 use crate::value::link::{PathSegment, TCPath};
+use crate::value::TCResult;
 
 pub type BlockId = PathSegment;
 
@@ -19,11 +21,11 @@ pub struct Dir {
 }
 
 impl Dir {
-    pub fn new(mount_point: PathBuf) -> Dir {
-        Dir {
+    pub fn new(mount_point: PathBuf) -> Arc<Dir> {
+        Arc::new(Dir {
             context: mount_point,
             children: HashMap::new(),
-        }
+        })
     }
 
     pub fn get_dir<'a>(&'a self, path: TCPath) -> BoxFuture<'a, Option<Arc<Dir>>> {
@@ -40,6 +42,31 @@ impl Dir {
                 None
             }
         })
+    }
+
+    pub fn new_dir<'a>(&'a mut self, path: TCPath) -> BoxFuture<'a, TCResult<Arc<Dir>>> {
+        Box::pin(async move {
+            if path.is_empty() {
+                Err(error::not_found(path))
+            } else if path.len() == 1 {
+                let path = path[0].clone();
+                if self.children.contains_key(&path) {
+                    Err(error::bad_request("Tried to create a new directory but there is already an entry at this path", &path))
+                } else {
+                    let dir = Dir::new(self.fs_path(&path));
+                    self.children.insert(path, DirEntry::Dir(dir.clone()));
+                    Ok(dir)
+                }
+            } else {
+                self.new_dir(path.slice_from(1)).await
+            }
+        })
+    }
+
+    fn fs_path(&self, name: &PathSegment) -> PathBuf {
+        let mut path = self.context.clone();
+        path.push(name.to_string());
+        path
     }
 }
 
