56b8ac428220b1f43ddd2aebeca086627113a9be
Haydn Vestal <haydnv@gmail.com>
2020-06-13 15:52:09 -0500

    include missing file

---
+++ src/value/op.rs
@@ -0,0 +1,46 @@
+use std::fmt;
+
+use super::link::Link;
+use super::{TCRef, Value};
+
+#[derive(Clone, Hash, Eq, PartialEq)]
+pub enum Subject {
+    Ref(TCRef),
+    Link(Link),
+}
+
+impl fmt::Display for Subject {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Subject::Ref(r) => write!(f, "{}", r),
+            Subject::Link(l) => write!(f, "{}", l),
+        }
+    }
+}
+
+impl From<TCRef> for Subject {
+    fn from(r: TCRef) -> Subject {
+        Subject::Ref(r)
+    }
+}
+
+impl From<Link> for Subject {
+    fn from(link: Link) -> Subject {
+        Subject::Link(link)
+    }
+}
+
+#[derive(Clone, Hash, Eq, PartialEq)]
+pub enum Op {
+    Get(Subject, Value),
+    Put(Subject, Value, Value),
+}
+
+impl fmt::Display for Op {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Op::Get(subject, id) => write!(f, "Op::Get {}: {}", subject, id),
+            Op::Put(subject, id, val) => write!(f, "Op::Put {}: {} <- {}", subject, id, val),
+        }
+    }
+}
