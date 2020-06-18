eac3c9976c7d68ce851d4c8138ea9f9a7141125f
Haydn Vestal <haydnv@gmail.com>
2020-06-18 10:23:04 -0500

    delete old Chain implementation

--- src/internal/mod.rs
+++ src/internal/mod.rs
@@ -1,16 +1,9 @@
 pub mod archive;
 pub mod cache;
-pub mod chain;
 pub mod lock;
 
 // TODO: DELETE
 mod file;
-
-// TODO: DELETE
-pub const RECORD_DELIMITER: char = 30 as char;
-
-// TODO: DELETE
-pub const GROUP_DELIMITER: char = 29 as char;
 
 pub type BlockId = file::BlockId;
 pub type File = file::File;
