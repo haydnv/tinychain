7820baa135adfeff2ae3a99f8180cb13d55631c9
Haydn Vestal <haydnv@gmail.com>
2020-08-24 13:19:58 -0500

    impl Transact for Chain

--- src/block/chain.rs
+++ src/block/chain.rs
@@ -1,13 +1,15 @@
 use std::convert::TryFrom;
 use std::sync::Arc;
 
+use async_trait::async_trait;
 use bytes::Bytes;
+use futures::join;
 
 use crate::class::TCResult;
 use crate::collection::Collect;
 use crate::error;
 use crate::transaction::lock::{Mutable, TxnLock};
-use crate::transaction::Txn;
+use crate::transaction::{Transact, Txn, TxnId};
 
 use super::file::File;
 use super::BlockData;
@@ -33,12 +35,35 @@ impl BlockData for ChainBlock {}
 
 pub struct Chain<O: Collect> {
     file: Arc<File<ChainBlock>>,
-    latest_block: TxnLock<Mutable<u64>>,
     object: O,
+    latest_block: TxnLock<Mutable<u64>>,
 }
 
 impl<O: Collect> Chain<O> {
-    pub async fn create(_txn: Arc<Txn>, _object: O) -> TCResult<Chain<O>> {
-        Err(error::not_implemented())
+    pub async fn create(txn: Arc<Txn>, object: O) -> TCResult<Chain<O>> {
+        let file = txn.context().await?;
+        let latest_block = TxnLock::new(txn.id().clone(), 0.into());
+        Ok(Chain {
+            file,
+            object,
+            latest_block,
+        })
+    }
+}
+
+#[async_trait]
+impl<O: Collect> Transact for Chain<O> {
+    async fn commit(&self, txn_id: &TxnId) {
+        self.object.commit(txn_id).await;
+        // don't commit the Chain until the actual changes are committed, for crash recovery
+        join!(self.file.commit(txn_id), self.latest_block.commit(txn_id));
+    }
+
+    async fn rollback(&self, txn_id: &TxnId) {
+        self.object.rollback(txn_id).await;
+        join!(
+            self.file.rollback(txn_id),
+            self.latest_block.rollback(txn_id)
+        );
     }
 }
