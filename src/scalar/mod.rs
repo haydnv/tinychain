46f4261fd7f39d68bd2d33890146f2e80dec521d
Haydn Vestal <haydnv@gmail.com>
2020-10-12 18:30:31 -0500

    fix bugs with GET Method parsing

--- src/scalar/mod.rs
+++ src/scalar/mod.rs
@@ -283,21 +283,17 @@ impl<T: TryFrom<Scalar, Error = error::TCError>> TryFrom<Scalar> for Vec<T> {
 impl TryCastFrom<Scalar> for Value {
     fn can_cast_from(scalar: &Scalar) -> bool {
         match scalar {
-            Scalar::Map(_map) => unimplemented!(),
-            Scalar::Object(_obj) => unimplemented!(),
-            Scalar::Op(_) => false,
             Scalar::Value(_) => true,
             Scalar::Tuple(tuple) => Value::can_cast_from(tuple),
+            _ => false,
         }
     }
 
     fn opt_cast_from(scalar: Scalar) -> Option<Value> {
         match scalar {
-            Scalar::Map(_map) => unimplemented!(),
-            Scalar::Object(_obj) => unimplemented!(),
-            Scalar::Op(_op) => None,
             Scalar::Value(value) => Some(value),
             Scalar::Tuple(tuple) => Value::opt_cast_from(tuple),
+            _ => None
         }
     }
 }
@@ -555,10 +551,11 @@ impl<'de> de::Visitor<'de> for ScalarVisitor {
                         TCPath::default(),
                     )
                 };
+                let value: Scalar = access.next_value()?;
 
-                return if data == Scalar::Tuple(vec![]) || data == Scalar::Value(Value::None) {
+                if value == Scalar::Tuple(vec![]) || value == Scalar::Value(Value::None) {
                     if path == TCPath::default() {
-                        Ok(Scalar::Value(Value::TCString(subject.into())))
+                        Ok(Scalar::Value(subject.into()))
                     } else {
                         Ok(Scalar::Op(Box::new(Op::Method(Method::Get(
                             (subject, path),
