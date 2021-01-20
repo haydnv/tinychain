769a003fb4639ec674126eaa1d8eb541dfc3e1ed
Haydn Vestal <haydnv@gmail.com>
2021-01-19 18:09:30 -0600

    add ValueType

--- scalar/value/src/lib.rs
+++ scalar/value/src/lib.rs
@@ -1,12 +1,56 @@
 use async_trait::async_trait;
 use destream::{FromStream, ToStream, Decoder, Encoder};
-use number_general::Number;
+use number_general::{Number, NumberType};
 
 use generic::{Map, Tuple};
 
 pub mod link;
 
 pub use link::*;
+use std::cmp::Ordering;
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub enum ValueType {
+    Link,
+    Map,
+    Number(NumberType),
+    String,
+    Tuple,
+    Value,
+}
+
+impl Ord for ValueType {
+    fn cmp(&self, other: &Self) -> Ordering {
+        use Ordering::*;
+
+        match (self, other) {
+            (l, r) if l == r => Equal,
+
+            (Self::Value, _) => Greater,
+            (_, Self::Value) => Less,
+
+            (Self::Map, _) => Greater,
+            (_, Self::Map) => Less,
+
+            (Self::Tuple, _) => Greater,
+            (_, Self::Tuple) => Less,
+
+            (Self::Link, _) => Greater,
+            (_, Self::Link) => Less,
+
+            (Self::String, _) => Greater,
+            (_, Self::String) => Less,
+
+            (Self::Number(l), Self::Number(r)) => l.cmp(r),
+        }
+    }
+}
+
+impl PartialOrd for ValueType {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
 
 #[derive(Clone, Eq, PartialEq)]
 pub enum Value {
