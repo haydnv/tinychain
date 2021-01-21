3746683519d858c45b90f6e86d00fb5ecfdf3d61
Haydn Vestal <haydnv@gmail.com>
2021-01-21 11:23:51 -0600

    add feature flag 'http'

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,55 @@
 use std::net::IpAddr;
 
 use async_trait::async_trait;
-use futures::join;
+use futures::future::{try_join_all, Future};
+use structopt::StructOpt;
 
+#[cfg(feature = "http")]
 pub mod http;
 pub mod state;
 pub mod txn;
 
-const DEFAULT_ADDR: &'static str = "127.0.0.1";
-const DEFAULT_PORT: u16 = 8702;
+#[derive(Clone, StructOpt)]
+struct Config {
+    #[structopt(long = "address", default_value = "127.0.0.1")]
+    pub address: IpAddr,
+
+    #[structopt(long = "http_port", default_value = "8702")]
+    pub http_port: u16,
+
+    #[structopt(long = "log_level", default_value = "warn")]
+    pub log_level: String,
+}
 
 #[async_trait]
 pub trait Server {
-    type Error;
+    type Error: std::error::Error;
 
     async fn listen(self) -> Result<(), Self::Error>;
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("warn")).init();
+    let config = Config::from_args();
 
-    let address: IpAddr = DEFAULT_ADDR.parse().unwrap();
-    let server = http::HTTPServer::new((address, DEFAULT_PORT).into());
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(config.log_level)).init();
 
-    if let (Err(cause),) = join!(server.listen()) {
+    #[allow(unused_mut)]
+    let mut servers = Vec::<Box<dyn Future<Output = Result<(), Box<dyn std::error::Error>>> + Unpin>>::with_capacity(1);
+
+    #[cfg(feature = "http")]
+    {
+        use futures::TryFutureExt;
+
+        let http_addr = (config.address, config.http_port).into();
+        let server = http::HTTPServer::new(http_addr);
+        servers.push(Box::new(server.listen().map_err(|e| {
+            let e: Box<dyn std::error::Error> = Box::new(e);
+            e
+        })));
+    }
+
+    if let Err(cause) = try_join_all(servers).await {
         log::error!("Server error: {}", cause);
     }
 
