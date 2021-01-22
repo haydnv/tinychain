763b5983f8423f7957ce4fb75ace9ad7d24626c9
Haydn Vestal <haydnv@gmail.com>
2021-01-21 18:13:16 -0600

    add hostfs package

---
+++ hostfs/src/lib.rs
@@ -0,0 +1,11 @@
+use std::path::PathBuf;
+
+use futures_locks::RwLock;
+
+mod dir;
+
+pub use dir::Dir;
+
+pub fn mount(mount_point: PathBuf) -> RwLock<Dir> {
+    RwLock::new(Dir::new(mount_point))
+}
