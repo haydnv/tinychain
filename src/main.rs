1ab036df9d723154236c19239245cc378dfee4de
Haydn Vestal <haydnv@gmail.com>
2021-01-21 19:34:01 -0600

    separate library from binary

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
 use std::net::IpAddr;
 
-use async_trait::async_trait;
 use futures::future::{try_join_all, Future};
 use structopt::StructOpt;
 
-pub mod block;
-
-#[cfg(feature = "http")]
-pub mod http;
-pub mod state;
-pub mod txn;
+use tinychain::{http, Server};
 
 #[derive(Clone, StructOpt)]
 struct Config {
@@ -21,13 +15,6 @@ struct Config {
 
     #[structopt(long = "log_level", default_value = "warn")]
     pub log_level: String,
-}
-
-#[async_trait]
-pub trait Server {
-    type Error: std::error::Error;
-
-    async fn listen(self) -> Result<(), Self::Error>;
 }
 
 #[tokio::main]
