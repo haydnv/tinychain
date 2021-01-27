ad1a33c8cf0bd53b51a1fb4156a50a0cd44fe43b
Haydn Vestal <haydnv@gmail.com>
2021-01-26 19:29:21 -0600

    add transact::Public trait

--- transact/src/lib.rs
+++ transact/src/lib.rs
@@ -3,12 +3,40 @@ use std::collections::HashSet;
 use async_trait::async_trait;
 
 use error::*;
-use generic::Id;
+use generic::{Id, Map, PathSegment};
 
 pub mod lock;
 mod txn;
 
 pub use txn::*;
+
+#[async_trait]
+pub trait Public {
+    type Key;
+    type State: Clone;
+
+    async fn get(
+        &self,
+        txn: &Txn<Self::State>,
+        path: &[PathSegment],
+        key: Self::Key,
+    ) -> TCResult<Self::State>;
+
+    async fn put(
+        &self,
+        txn: &Txn<Self::State>,
+        path: &[PathSegment],
+        key: Self::Key,
+        value: Self::State,
+    ) -> TCResult<()>;
+
+    async fn post(
+        &self,
+        txn: &Txn<Self::State>,
+        path: &[PathSegment],
+        params: Map<Self::State>,
+    ) -> TCResult<Self::State>;
+}
 
 pub trait State: Clone + Sized + Send {
     fn is_ref(&self) -> bool;
