27bcd570a91d60e3822537470c9e1bf357e7c5d3
Haydn Vestal <haydnv@gmail.com>
2021-03-30 16:05:38 -0500

    impl Transact for BlockChain

--- host/src/chain/blockchain.rs
+++ host/src/chain/blockchain.rs
@@ -1,7 +1,9 @@
 use async_trait::async_trait;
+use futures::join;
 
 use tc_error::*;
-use tc_transact::fs::Persist;
+use tc_transact::fs::{BlockData, File, Persist};
+use tc_transact::lock::{Mutable, TxnLock};
 use tc_transact::Transact;
 use tcgeneric::TCPathBuf;
 
@@ -9,24 +11,32 @@ use crate::fs;
 use crate::scalar::{Link, Scalar, Value};
 use crate::txn::{Txn, TxnId};
 
-use super::{ChainInstance, Schema, Subject};
+use super::{ChainBlock, ChainInstance, Schema, Subject};
+
+const BLOCK_SIZE: u64 = 1_000_000;
 
 #[derive(Clone)]
 pub struct BlockChain {
     schema: Schema,
     subject: Subject,
+    latest: TxnLock<Mutable<u64>>,
+    file: fs::File<ChainBlock>,
 }
 
 #[async_trait]
 impl ChainInstance for BlockChain {
     async fn append(
         &self,
-        _txn_id: TxnId,
-        _path: TCPathBuf,
-        _key: Value,
-        _value: Scalar,
+        txn_id: TxnId,
+        path: TCPathBuf,
+        key: Value,
+        value: Scalar,
     ) -> TCResult<()> {
-        Err(TCError::not_implemented("BlockChain::append"))
+        let latest = self.latest.read(&txn_id).await?;
+        let mut block = self.file.write_block(txn_id, (*latest).into()).await?;
+
+        block.append(txn_id, path, key, value);
+        Ok(())
     }
 
     fn subject(&self) -> &Subject {
@@ -54,11 +64,32 @@ impl Persist for BlockChain {
 
 #[async_trait]
 impl Transact for BlockChain {
-    async fn commit(&self, _txn_id: &TxnId) {
-        unimplemented!()
+    async fn commit(&self, txn_id: &TxnId) {
+        {
+            let latest = self.latest.read(txn_id).await.expect("latest block number");
+
+            let block = self
+                .file
+                .read_block(txn_id, &(*latest).into())
+                .await
+                .expect("read latest chain block");
+
+            if block.size().await.expect("block size") >= BLOCK_SIZE {
+                let mut latest = latest.upgrade().await.expect("latest block number");
+                (*latest) += 1;
+
+                self.file
+                    .create_block(*txn_id, (*latest).into(), ChainBlock::new())
+                    .await
+                    .expect("bump chain block number");
+                // TODO: include the hash of the last block in the new latest block
+            }
+        }
+
+        join!(self.latest.commit(txn_id), self.file.commit(txn_id));
     }
 
-    async fn finalize(&self, _txn_id: &TxnId) {
-        unimplemented!()
+    async fn finalize(&self, txn_id: &TxnId) {
+        join!(self.latest.finalize(txn_id), self.file.finalize(txn_id));
     }
 }
