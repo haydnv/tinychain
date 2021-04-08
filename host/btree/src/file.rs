2aa1efc0d1541c4635c7db28144221c053d6f050
Haydn Vestal <haydnv@gmail.com>
2021-04-08 13:37:10 -0500

    define BTree NodeKey

--- host/btree/src/file.rs
+++ host/btree/src/file.rs
@@ -1,2 +1,57 @@
+use std::fmt;
+use std::iter::FromIterator;
+use std::ops::Deref;
+
+use async_trait::async_trait;
+use destream::de;
+use futures::TryFutureExt;
+
+use tc_value::Value;
+
+#[derive(Clone)]
+struct NodeKey {
+    deleted: bool,
+    value: Vec<Value>,
+}
+
+impl NodeKey {
+    fn new(value: Vec<Value>) -> Self {
+        Self {
+            deleted: false,
+            value,
+        }
+    }
+}
+
+impl Deref for NodeKey {
+    type Target = [Value];
+
+    fn deref(&self) -> &[Value] {
+        &self.value
+    }
+}
+
+#[async_trait]
+impl de::FromStream for NodeKey {
+    type Context = ();
+
+    async fn from_stream<D: de::Decoder>(cxt: (), decoder: &mut D) -> Result<Self, D::Error> {
+        de::FromStream::from_stream(cxt, decoder)
+            .map_ok(|(deleted, value)| Self { deleted, value })
+            .await
+    }
+}
+
+impl fmt::Display for NodeKey {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "BTree node key: {}{}",
+            Value::from_iter(self.value.to_vec()),
+            if self.deleted { " (DELETED)" } else { "" }
+        )
+    }
+}
+
 #[derive(Clone)]
 pub struct BTreeFile {}
