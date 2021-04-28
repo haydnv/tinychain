d3b894f9fc65b3173b5a63b7d0d31cd2252351f0
Haydn Vestal <haydnv@gmail.com>
2021-04-27 19:32:28 -0500

    route /count endpoint for Table

--- host/src/route/collection/table.rs
+++ host/src/route/collection/table.rs
@@ -1,12 +1,56 @@
+use futures::TryFutureExt;
+
+use tc_btree::Node;
+use tc_error::*;
+use tc_table::TableInstance;
+use tc_transact::Transaction;
 use tcgeneric::PathSegment;
 
 use crate::collection::Table;
+use crate::fs::{Dir, File};
+use crate::route::{GetHandler, Handler, Route};
+use crate::state::State;
+use crate::txn::Txn;
 
-use super::{Handler, Route};
+struct CountHandler<T> {
+    table: T,
+}
+
+impl<'a, T: TableInstance<File<Node>, Dir, Txn> + 'a> Handler<'a> for CountHandler<T> {
+    fn get(self: Box<Self>) -> Option<GetHandler<'a>> {
+        Some(Box::new(|txn, key| {
+            Box::pin(async move {
+                if key.is_some() {
+                    return Err(TCError::bad_request(
+                        "Table::count does not accept a key (call Table::slice first)",
+                        key,
+                    ));
+                }
+
+                self.table.count(*txn.id()).map_ok(State::from).await
+            })
+        }))
+    }
+}
+
+impl<T> From<T> for CountHandler<T> {
+    fn from(table: T) -> Self {
+        Self { table }
+    }
+}
 
 impl Route for Table {
-    fn route<'a>(&'a self, _path: &'a [PathSegment]) -> Option<Box<dyn Handler<'a> + 'a>> {
-        // TODO
-        None
+    fn route<'a>(&'a self, path: &'a [PathSegment]) -> Option<Box<dyn Handler<'a> + 'a>> {
+        if path.is_empty() {
+            // TODO
+            return None;
+        } else if path.len() == 1 {
+            match path[0].as_str() {
+                "count" => Some(Box::new(CountHandler::from(self.clone()))),
+                _ => None,
+            }
+        } else {
+            None
+        }
     }
 }
