c44e0c025e881c8553c9c4a764d39e3c5e7677d4
Haydn Vestal <haydnv@gmail.com>
2021-04-28 11:15:49 -0500

    add TableTests.testInsert

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,6 +1,8 @@
+import random
 import tinychain as tc
 import unittest
 
+from num2words import num2words
 from testutils import start_host, PORT
 
 
@@ -18,10 +20,26 @@ class TableTests(unittest.TestCase):
     def testCreate(self):
         cxt = tc.Context()
         cxt.table = tc.Table(SCHEMA)
-        cxt.result = tc.After(cxt.table.insert(("Movie Name",), (0,)), cxt.table.count())
+        cxt.result = tc.After(cxt.table.insert(("name",), (0,)), cxt.table.count())
 
         count = self.host.post(ENDPOINT, cxt)
         self.assertEqual(count, 1)
+
+    def testInsert(self):
+        for x in range(0, 100, 10):
+            keys = list(range(x))
+            random.shuffle(keys)
+
+            cxt = tc.Context()
+            cxt.table = tc.Table(SCHEMA)
+            cxt.inserts = [
+                cxt.table.insert((num2words(i),), (i,))
+                for i in keys]
+
+            cxt.result = tc.After(cxt.inserts, cxt.table.count())
+
+            result = self.host.post(ENDPOINT, cxt)
+            self.assertEqual(result, x)
 
 
 if __name__ == "__main__":
