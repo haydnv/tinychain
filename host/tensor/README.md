4c9884e88edd08668cdaaba78af361dc7dd0244d
Haydn Vestal <haydnv@gmail.com>
2021-05-20 10:32:42 -0500

    add tensor sub-crate & feature

---
+++ host/tensor/README.md
@@ -0,0 +1,15 @@
+This crate is used internally by Tinychain to provide the Tensor data structure. You can enable it using the `tensor` feature (e.g. `cargo build --release --features=tensor`). It requires linking to [ArrayFire](http://arrayfire.com) version 3.8 in order to compile. You can download and install ArrayFire by following the instructions at [http://arrayfire.org/docs/installing.htm](http://arrayfire.org/docs/installing.htm). You'll also have to add a package config file to your `$PKG_CONFIG_PATH` like so:
+
+```
+prefix=/usr
+exec_prefix=${prefix}
+includedir=${prefix}/include
+libdir=${exec_prefix}/lib64
+
+Name: arrayfire
+Description: the ArrayFire library
+Version: 3.8
+Libs: -L${libdir}
+```
+
+For more information on Tinychain, see [http://github.com/haydnv/tinychain](http://github.com/haydnv/tinychain)
---
+++ host/tensor/src/lib.rs
@@ -0,0 +1,41 @@
+use std::fmt;
+
+use tcgeneric::{path_label, Class, Instance, NativeClass, PathLabel, PathSegment, TCPathBuf};
+
+const PREFIX: PathLabel = path_label(&["state", "collection", "tensor"]);
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+pub enum TensorType {}
+
+impl Class for TensorType {}
+
+impl NativeClass for TensorType {
+    fn from_path(path: &[PathSegment]) -> Option<Self> {
+        if path.len() > 3 && &path[..3] == &PREFIX[..] {
+            todo!()
+        } else {
+            None
+        }
+    }
+
+    fn path(&self) -> TCPathBuf {
+        PREFIX.into()
+    }
+}
+
+impl fmt::Display for TensorType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("type Tensor")
+    }
+}
+
+#[derive(Clone)]
+pub enum Tensor {}
+
+impl Instance for Tensor {
+    type Class = TensorType;
+
+    fn class(&self) -> Self::Class {
+        todo!()
+    }
+}
