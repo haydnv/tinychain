aac1c15f54e24f5e1d88062b1ac4487932b4523b
Haydn Vestal <haydnv@gmail.com>
2021-07-19 11:20:45 -0500

    test creating a node in a Graph

--- client/tinychain/app.py
+++ client/tinychain/app.py
@@ -1,4 +1,7 @@
+from tinychain.chain import Sync
 from tinychain.cluster import Cluster
+from tinychain.collection.schema import Graph as Schema
+from tinychain.collection.table import Table
 from tinychain.collection.tensor import Sparse
 from tinychain.value import Bool
 
@@ -11,14 +14,22 @@ class Graph(Cluster):
             if hasattr(self, name):
                 raise ValueError(f"Graph already has an entry called f{name}")
 
-            setattr(self, name, schema.tables[name])
+            setattr(self, name, schema.chain(Table(schema.tables[name])))
 
         for name in schema.edges:
+            (from_node, to_node) = schema.edges[name]
+
+            if '.' not in from_node:
+                raise ValueError(f"{from_node} must specify a column")
+
+            if '.' not in to_node:
+                raise ValueError(f"{to_node} must specify a column")
+
             tensor_name = f"edges_{name}"
             if hasattr(self, tensor_name):
                 raise ValueError(f"Graph already has an entry called f{name}")
 
-            setattr(self, tensor_name, Sparse(Bool, [0, 0]))
+            setattr(self, tensor_name, schema.chain(Sparse.zeros([0, 0], Bool)))
 
     def _schema(self):
-        raise NotImplementedError("you must override the Graph._schema method")
+        return Schema(Sync)
--- tests/test_app.py
+++ tests/test_app.py
@@ -5,14 +5,43 @@ from testutils import start_host
 
 
 class TestApp(tc.Graph):
+    __uri__ = tc.URI("/test/graph")
+
     def _schema(self):
-        return tc.schema.Graph()
+        users = tc.schema.Table(
+            [tc.Column("user_id", tc.Bytes, 16)],
+            [tc.Column("email", tc.String, 320), tc.Column("display_name", tc.String, 100)])
+
+        products = tc.schema.Table(
+            [tc.Column("sku", tc.U32)],
+            [tc.Column("name", tc.String, 256), tc.Column("price", tc.U32)])
+
+        orders = tc.schema.Table(
+            [tc.Column("order_id", tc.Bytes, 16)],
+            [tc.Column("user_id", tc.Bytes, 16), tc.Column("sku", tc.U32), tc.Column("quantity", tc.UInt)])
+
+        schema = (tc.schema.Graph(tc.chain.Block)
+                  .create_table("users", users)
+                  .create_table("products", products)
+                  .create_table("orders", orders)
+                  .create_edge("sku", "products.sku", "orders.sku")
+                  .create_edge("orders", "users.user_id", "orders.user_id"))
+
+        return schema
+
+    @tc.post_method
+    def create_user(self, _txn, user_id: tc.Bytes, email: tc.String, display_name: tc.String):
+        self.users.insert({"user_id": user_id, "email": email, "display_name": display_name})
 
 
 class AppTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.host = start_host("test_app", [TestApp], overwrite=True)
+
+    def testCreateUser(self):
+        user = {"user_id": "MTIzNDU=", "email": "user12345@example.com", "display_name": "user 12345"}
+        self.host.post("/test/graph/create_user", user)
 
     @classmethod
     def tearDownClass(cls):
