532836a815e8488efe2f7815cc34b99adf44ef7c
haydnv <haydnv@gmail.com>
2021-10-04 11:43:08 -0500

    bump client version

--- client/docs/conf.py
+++ client/docs/conf.py
@@ -17,9 +17,9 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Tinychain'
+project = 'TinyChain'
 copyright = '2021'
-author = 'The Tinychain Contributors'
+author = 'The TinyChain Contributors'
 
 # The full version, including alpha/beta/rc tags
 release = '0.4'
