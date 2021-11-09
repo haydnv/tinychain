671fa84d1760b04ea490a4daaa7e82c4765e131c
haydnv <haydnv@gmail.com>
2021-11-09 11:53:25 +0530

    implement Conv2D.eval

--- client/tinychain/__init__.py
+++ client/tinychain/__init__.py
@@ -4,7 +4,7 @@ import tinychain.error
 import tinychain.graph
 import tinychain.graph.edge
 import tinychain.host
-import tinychain.ml.conv
+import tinychain.ml.cnn
 import tinychain.ml.dnn
 import tinychain.op
 import tinychain.ref
