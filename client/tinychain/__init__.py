9270c756aa442ddfa66c1a4536663fc55dd9e2f5
haydnv <haydnv@gmail.com>
2022-02-02 10:45:49 -0500

    remove obsolete imports from client __init__.py file

--- client/tinychain/__init__.py
+++ client/tinychain/__init__.py
@@ -4,8 +4,6 @@ import tinychain.error
 import tinychain.graph
 import tinychain.graph.edge
 import tinychain.host
-import tinychain.ml.cnn
-import tinychain.ml.dnn
 import tinychain.ml.nn
 import tinychain.ml.optimizer
 import tinychain.op
