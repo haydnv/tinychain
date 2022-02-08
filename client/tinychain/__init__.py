d4993f103b2639b974eabb07cc993013fb759a1b
haydnv <haydnv@gmail.com>
2022-02-08 17:12:21 +0100

    remove unused Object class from reflect package

--- client/tinychain/__init__.py
+++ client/tinychain/__init__.py
@@ -15,7 +15,7 @@ from tinychain.collection import btree, table, tensor
 from tinychain.decorators import closure, delete_method, get_method, post_method, put_method, delete_op, get_op, post_op, put_op
 from tinychain.ml import linalg
 from tinychain.ref import After, Case, If, While
-from tinychain.reflect import Meta, Object
+from tinychain.reflect import Meta
 from tinychain.state import Class, Instance, Map, State, Scalar, Stream, Tuple
 from tinychain.value import *
 from tinychain.util import form_of, print_json, to_json, uri, use, Context, URI
