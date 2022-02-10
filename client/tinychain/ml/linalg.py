1be9983b8a19ed2d862b46a86e9d4dcd8a84a100
KOHbDSsource <dimastr2110@gmail.comsource>
2022-02-10 19:06:45 +0300

    SVD

--- client/tinychain/ml/linalg.py
+++ client/tinychain/ml/linalg.py
@@ -2,7 +2,7 @@ from tinychain.collection.tensor import einsum, Dense, Schema, Sparse, Tensor
 from tinychain.decorators import closure, get_op, post_op
 from tinychain.ref import After, Get, If, MethodSubject, While
 from tinychain.state import Map, Stream, Tuple
-from tinychain.value import Bool, F64, UInt, F32, Int
+from tinychain.value import Bool, F64, UInt, F32, Int, Value, Number
 
 # from "Numerical Recipes in C" p. 65
 EPS = 10**-6
@@ -39,13 +39,13 @@ def householder(cxt, x: Tensor) -> Tuple:
     """Compute the Householder vector of the given column vector `a`."""
 
     cxt.alpha = x[0]
-    cxt.s = (x[1:]**2).sum()
+    cxt.s = F32(If(Int(x.shape[0]) > 1, (x[1:]**2).sum(), 0.0))
     cxt.t = (cxt.alpha**2 + cxt.s)**0.5
 
-    cxt.v = x.copy()  # make a copy in case X is updated before the return values are evaluated
+    cxt.v = x.copy()
     cxt.v_zero = F64(If(cxt.alpha <= 0, cxt.alpha - cxt.t, -cxt.s / (cxt.alpha + cxt.t)))
     tau = If(cxt.s.abs() < EPS, 0, 2 * cxt.v_zero**2 / (cxt.s + cxt.v_zero ** 2))
-    v = After(cxt.v[0].write(cxt.v_zero), cxt.v / cxt.v_zero)
+    v = Tensor(If(Int(x.shape[0]) > 1, After(cxt.v[0].write(cxt.v_zero), cxt.v / cxt.v_zero), cxt.v))
 
     return v, tau
 
@@ -102,18 +102,17 @@ def qr(cxt, x: Tensor) -> Tuple:
     @closure(outer_cxt.m, cxt.householder)
     @post_op
     def qr_step(cxt, Q: Tensor, R: Tensor, k: UInt) -> Map:
-        cxt.v, cxt.tau = outer_cxt.householder(x=R[k:, k]).unpack(2)
-        cxt.v_outer = einsum("i,j->ij", [cxt.v, cxt.v])
-
+        cxt.v, cxt.tau = Tuple(outer_cxt.householder(x=R[k:, k])).unpack(2)
+        cxt.v_outer = Tensor(einsum("i,j->ij", [Tensor(cxt.v), Tensor(cxt.v)]))
         cxt.H = identity(outer_cxt.m, F64).as_dense().copy()
-        cxt.H_sub = (cxt.H[k:, k:] - (cxt.v_outer * cxt.tau))
+        cxt.H_sub = Tensor(cxt.H[k:, k:] - (Tensor(cxt.v_outer) * cxt.tau))
         return After(cxt.H[k:, k:].write(cxt.H_sub), {
             "Q": matmul(cxt.H, Q),
             "R": matmul(cxt.H, R),
         })
 
-    state = Map(Q=identity(cxt.m, F64).as_dense(), R=x)
-    QR = Stream.range(cxt.n - 1).fold("k", state, qr_step)
+    state = Map(Q=identity(cxt.m, F64).as_dense(), R=x.copy())
+    QR = Stream.range(cxt.n).fold("k", state, qr_step)
     return Tensor(QR['Q'])[:cxt.n].transpose(), Tensor(QR['R'])[:cxt.n]
 
 
@@ -269,7 +268,67 @@ def slogdet(cxt, x: Dense) -> Tuple:
     return After(Stream.range((0, n)).for_each(step), Tuple([cxt.sign_result, cxt.logdet_result]))
 
 
-def svd(matrix: Tensor) -> Tuple:
-    """Return the singular value decomposition of the given `matrix`."""
+@post_op
+def svd(cxt, A: Tensor, l=UInt(0), epsilon=F32(1e-5), max_iter=UInt(1000)) -> Tuple:
+    cxt.shape = A.shape
+    cxt.n_orig, cxt.m_orig = [UInt(dim) for dim in cxt.shape.unpack(2)]
+    k = Int(If(l == UInt(0), Value.min(Int(cxt.n_orig), Int(cxt.m_orig)), l))
+    A_orig = Tensor(A).copy()
+    cxt.A1, n, m = Tuple(If(
+        UInt(cxt.n_orig) > UInt(cxt.m_orig),
+        Tuple([Tensor(matmul(Tensor(A).transpose(), A)), Number(Tensor(A).shape[1]), Number(Tensor(A).shape[1])]),
+        Tuple(If(
+            UInt(cxt.n_orig) < UInt(cxt.m_orig),
+            Tuple([Tensor(matmul(A, Tensor(A).transpose())), Number(Tensor(A).shape[0]), Number(Tensor(A).shape[0])]),
+            Tuple([A, cxt.n_orig, cxt.m_orig])
+        )),
+    )).unpack(3)
 
-    raise NotImplementedError("singular value decomposition")
+    Q = Dense.random_uniform([n, k]).abs()
+    cxt.qr = qr
+    Q, R = cxt.qr(x=Q).unpack(2)
+
+    @closure(cxt.qr, cxt.A1)
+    @post_op
+    def step(i: UInt, Q_prev: Tensor, Q: Tensor, R: Tensor, err: F32):
+        Z = Tensor(matmul(Tensor(cxt.A1), Tensor(Q)))
+        _Q, _R = cxt.qr(x=Z).unpack(2)
+        _err = F32(Tensor(_Q).sub(Q_prev).pow(2).sum())
+        _Q_prev = Tensor(_Q).copy()
+        return Map(i=i + 1, Q_prev=_Q_prev, Q=Tensor(_Q), R=Tensor(_R), err=_err)
+
+    @closure(epsilon, max_iter)
+    @post_op
+    def cond(i: UInt, err: F32):
+        return (F32(err).abs() > epsilon).logical_and(i < max_iter)
+
+    result_loop = Map(While(cond, step, Map(
+        i=UInt(0),
+        Q_prev=Tensor(Q).copy(),
+        Q=Tensor(Q).copy(),
+        R=Tensor(R),
+        err=F32(1.0)
+        )))
+    Q, R = result_loop['Q'], result_loop['R']
+
+    singular_values = Tensor(Tensor(diagonal(R)).pow(0.5))
+    cxt.eye = identity(singular_values.shape[0], F32).as_dense().copy()
+    cxt.inv_matrix = (cxt.eye * singular_values.pow(-1))
+    cxt.Q_T = Tensor(Q).transpose()
+    cxt.vec_sing_values_upd = Map(If(
+        cxt.n_orig == cxt.m_orig,
+        Map(left_vecs=cxt.Q_T, right_vecs=cxt.Q_T, singular_values=(singular_values).pow(2)),
+        Map(
+            left_vecs=einsum('ij,jk->ik', [einsum('ij,jk->ik', [A_orig, Q]), cxt.inv_matrix]),
+            right_vecs=cxt.Q_T,
+            singular_values=singular_values)))
+    vec_sing_values = Map(If(
+        cxt.n_orig < cxt.m_orig,
+        Map(
+            left_vecs=cxt.Q_T,
+            right_vecs=einsum('ij,jk->ik', [einsum('ij,jk->ik', [cxt.inv_matrix, Q]), A_orig]),
+            singular_values=singular_values),
+            cxt.vec_sing_values_upd
+            ))
+
+    return vec_sing_values['left_vecs'], vec_sing_values['singular_values'], vec_sing_values['right_vecs']
