ef80a3a9fac3ca0a1ac7051459f1f32b8e47d6a7
MuhamedEssam <31318098+MuhamedEssam@users.noreply.github.com>
2022-02-17 19:11:57 +0200

    Add files via upload

--- client/tinychain/ml/mlr.py
+++ client/tinychain/ml/mlr.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Feb  6 16:59:54 2022
-@author: meracoda
-"""
+
 
 from tinychain.collection.tensor import einsum, Dense , Tensor
 from tinychain.ml import Layer, NeuralNet
@@ -21,20 +16,26 @@ class MultipleLinearRegression :
     @property
     def learningrate(self):  
         '''
-        We use property accessor so we recall self.learningrate instead of Dense(self[0])
+        We use property accessor so we recall self.learningrate instead of tc.Number(self[0])
         '''
-        return Dense(self[0])
+        return tc.Number(self[0])
     
     def predict(self, inputs, weights, learning = True):
         if learning==True:
             preds = einsum("ij,ki->kj", [weights, inputs]) 
         else:
-            #inputs = np.insert(inputs ,0,1,axis=1)   
-            inputs_=Tensor.concatenate(1,inputs,axis=1) #Question: i try to add column of ones ! 
+            '''
+            I try to add column of ones ! 
+            '''
+ 
+            inputs_=Tensor.concatenate(1,inputs,axis=1) 
             preds = einsum("ij,ki->kj", [weights, inputs_])            
         return preds
     
-    def cost_func( self, labels ,preds):#Question: now would it work ?
+    def cost_func( self, labels ,preds):
+        '''
+        I try to apply Mean Squared Error
+        '''
         cost = Tensor.sum((labels - preds)**2)/labels.len()
         return cost
 
@@ -48,42 +49,45 @@ class MultipleLinearRegression :
         new_weights = weights - self.learningrate * d_weights
         return new_weights
 
-    def fit(self, X_train, y_train):
+    def fit(self, X_train, y_train,iteration):
         X_train_ = Tensor.concatenate(1,X_train,axis=1) 
         y_train_ = y_train
         n_samples, n_features = X_train_.shape[1],X_train_.shape[0]
-        
-        weights = np.zeros((n_features + 1, 1)) #Q: Can i create zeros tc tensor  or i have to create it in numpy then convert?
+        '''
+        Can i create zeros tc tensor  or i have to create it in numpy then convert?
+        '''       
+        weights = np.zeros((n_features + 1, 1)) 
         weights_= Tensor.load(weights.shape,tc.F32,weights.flatten().tolist())        
         
  
         @tc.get_op
-        def loop(until: tc.Number) -> tc.Int:
+        def loop(iteration: tc.Number) -> tc.Int:
             @tc.closure
             @tc.post_op
             def cond(i: tc.Int):
-                return i < until
+                return i < iteration
         ​
             @tc.post_op
             def step(i: tc.Int) -> tc.Int:
   
-                preds=self.predict(X_train_,weights_)
+                preds=self.predict(X_train_,weights_,learning=True)
                 cost_=self.cost_func(y_train_,preds)
 
-                d_weights=gradient_descent(X_train_,weights_,y_train_)
+                d_weights=self.gradient_descent(X_train_,weights_,y_train_)
                 weights_=self.update_params(weights_,d_weights)
                 
-                return tc.Map(i=i + 1 ,cost_)  
+                return tc.Map(i=i + 1 ,cost_,weights_)  
         ​
-            initial_state = tc.Map(i=0)  
+            initial_state = tc.Map(i=0,cost_=0,weights_=weights_)  
         ​
             # return the loop itself
             return tc.While(cond, step, initial_state)
+        return weights_
             
 
-    def R2_Score(self, x, y):
-        ss_res = Tensor.sum((self.predict(x, learning = False) - y)**2)
-        ss_tot = Tensor.sum((y - np.mean(y))**2)
+    def R2_Score(self, inputs, labels):
+        ss_res = Tensor.sum((self.predict(inputs, learning = False) - y)**2)
+        ss_tot = Tensor.sum((labels - Tensor.mean(labels))**2)
         r2 = 1 - ss_res/ss_tot
         return r2   
     
