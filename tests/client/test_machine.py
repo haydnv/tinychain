29fd3db95f952e75d42ef3551cac2cf1d238e424
MuhamedEssam <31318098+MuhamedEssam@users.noreply.github.com>
2022-02-21 15:46:42 +0200

    Add files via upload

---
+++ tests/client/test_machine.py
@@ -0,0 +1,68 @@
+import numpy as np
+import tinychain as tc
+import unittest
+from sklearn.model_selection import train_test_split
+
+from testutils import ClientTest
+
+Dense = tc.tensor.Dense
+
+ENDPOINT = "/transact/hypothetical"
+LEARNING_RATE = tc.F32(0.01)
+MAX_ITERATIONS = 10
+NUM_EXAMPLES = 20
+
+
+class Logistic_Regression():
+        # ?? no init right ?  so how can it work out ? should i inherit something ? 
+    def create (self,inputs):
+        layer0= tc.ml.nn.DNNLayer.create('layer0', inputs.shape[1], 1, tc.ml.Sigmoid())
+        return tc.ml.nn.Sequential.load([layer0])
+    
+    def cost(self,output, labels, dl=False):
+        if dl:
+            return output.sub(labels).mul(2).mean()
+        return output.sub(labels).pow(2).mean()
+    
+    def fit (self,inputs,labels):
+        def train_while(i: tc.UInt, output: tc.tensor.Dense):
+            return (i <= MAX_ITERATIONS).logical_and(((output > 0.5) != labels).any())
+        # for trial only , Adam must be replaced by Gradient Descent !
+        optimizer = tc.ml.optimizer.Adam.create(param_list=logistic.get_param_list(), lr=LEARNING_RATE)
+        result= tc.ml.optimizer.train(logistic, optimizer, inputs, labels, cost, train_while)
+        return result
+        
+class MLTests(ClientTest):
+    def test_logistic(self):
+        cxt=tc.Context()
+        data= np.array([[11,-2,1],
+               [3,-4,0],
+               [2,-10,0],
+               [10,-5,1],
+               [1,-3,0],
+               [6,-5,1],
+               [2,-7,0],
+               [6,-1,1]])
+        X=data[:,:-1]
+        y=data[:,-1]
+        
+        X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0,test_size=0.2)
+
+        
+        cxt.inputs=load(X_train)
+        cxt.labels=load(y_train)
+        
+        cxt.logistic = Logistic_Regression.create(cxt.inputs)
+        
+        cxt.result=cxt.logistic.fit(cxt.inputs,cxt.labels)
+        
+        response = self.host.post(ENDPOINT, cxt)
+        self.assertEqual(response["i"], MAX_ITERATIONS + 1)
+
+
+def load(ndarray, dtype=tc.F32):
+    return tc.tensor.Dense.load(ndarray.shape, dtype, ndarray.flatten().tolist())
+
+
+if __name__ == "__main__":
+    unittest.main()
