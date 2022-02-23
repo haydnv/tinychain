6eb488cbae12209975a809899940cbc649e46e3e
haydnv <haydnv@gmail.com>
2022-02-23 10:17:46 +0000

    add test for creating & evaluating a 2-layer convolutional net

--- tests/apps/test_neural_net.py
+++ tests/apps/test_neural_net.py
@@ -5,8 +5,16 @@ import tinychain as tc
 
 
 URI = tc.URI("/test/ml/app")
-DNN_LAYER_SHAPE = [[2, 2], [2, 1]]
 BATCH_SIZE = 1
+
+DNN_SHAPE = [[2, 2], [2, 1]]
+
+CNN_SHAPE = [
+    [[3, 5, 5], [2, 1, 1]],
+    [[2, 3, 3], [1, 3, 3]],
+]
+PADDING = 1
+STRIDE = 1
 
 
 class TestLibrary(tc.app.Library):
@@ -20,7 +28,8 @@ class TestLibrary(tc.app.Library):
 
     @tc.post_method
     def check_conv_layer(self, cxt, inputs: tc.tensor.Tensor) -> tc.tensor.Tensor:
-        cxt.layer = self.ML.ConvLayer.create((4, 3, 1), (1, 1, 1), 0, 1)
+        input_shape, output_shape = CNN_SHAPE[0]
+        cxt.layer = self.ML.ConvLayer.create(input_shape, output_shape, PADDING, STRIDE)
         return cxt.layer.forward(inputs=inputs)
 
     @tc.post_method
@@ -29,35 +38,63 @@ class TestLibrary(tc.app.Library):
         return cxt.layer.forward(inputs=inputs)
 
     @tc.post_method
+    def check_cnn(self, cxt, inputs: tc.tensor.Tensor) -> tc.tensor.Tensor:
+        cxt.nn = self.create_cnn()
+        return cxt.nn.forward(inputs=inputs)
+
+    @tc.get_method
+    def create_cnn(self) -> tc.hosted_ml.nn.NeuralNet:
+        layers = tc.Tuple([self.ML.ConvLayer.create(i, o, PADDING, STRIDE) for i, o in CNN_SHAPE])
+        return self.ML.Sequential(layers)
+
+    @tc.post_method
     def check_dnn(self, cxt, inputs: tc.tensor.Tensor) -> tc.tensor.Tensor:
-        cxt.dnn = self.create_dnn()
-        return cxt.dnn.forward(inputs=inputs)
+        cxt.nn = self.create_dnn()
+        return cxt.nn.forward(inputs=inputs)
 
     @tc.get_method
     def create_dnn(self) -> tc.hosted_ml.nn.NeuralNet:
-        layers = tc.Tuple([self.ML.DNNLayer.create(i, o, self.ML.Sigmoid()) for i, o in DNN_LAYER_SHAPE])
+        layers = tc.Tuple([self.ML.DNNLayer.create(i, o, self.ML.Sigmoid()) for i, o in DNN_SHAPE])
         return self.ML.Sequential(layers)
 
 
 class LibraryTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.host = testutils.start_host("test_neural_net", [tc.hosted_ml.service.ML(), TestLibrary()])
+        cls.host = testutils.start_docker("test_neural_net", [tc.hosted_ml.service.ML(), TestLibrary()])
 
     def testConvLayer(self):
-        inputs = load_dense(np.arange(12*BATCH_SIZE).reshape((BATCH_SIZE, 4, 3, 1)))
-        print(self.host.post(URI.append("check_conv_layer"), {"inputs": inputs}))
+        input_shape = [BATCH_SIZE] + CNN_SHAPE[0][0]
+        inputs = np.random.random(np.product(input_shape)).reshape(input_shape)
+        output = self.host.post(URI.append("check_conv_layer"), {"inputs": load_dense(inputs)})
+        self.assertEqual(output_shape(output), [BATCH_SIZE, 2, 3, 3])
 
-    def testDNNLayer(self):
-        print(self.host.post(URI.append("check_dnn_layer"), {"inputs": tc.tensor.Dense.load([1, 2], tc.F32, [0, 1])}))
+    def testCNN(self):
+        input_shape = [BATCH_SIZE] + CNN_SHAPE[0][0]
+        inputs = np.random.random(np.product(input_shape)).reshape(input_shape)
+        tc.print_json(self.host.get(URI.append("create_cnn")))
+        output = self.host.post(URI.append("check_cnn"), {"inputs": load_dense(inputs)})
+        self.assertEqual(output_shape(output), [BATCH_SIZE, 2, 1, 1])
 
     def testDNN(self):
-        tc.print_json(self.host.get(URI.append("create_dnn")))
-        print(self.host.post(URI.append("check_dnn"), {"inputs": tc.tensor.Dense.load([1, 2], tc.F32, [0, 1])}))
+        input_shape = [BATCH_SIZE, DNN_SHAPE[0][0]]
+        inputs = np.random.random(np.product(input_shape)).reshape(input_shape)
+        output = self.host.post(URI.append("check_dnn"), {"inputs": load_dense(inputs)})
+        self.assertEqual(output_shape(output), [BATCH_SIZE, 2])
+
+    def testDNNLayer(self):
+        input_shape = [BATCH_SIZE, DNN_SHAPE[0][0]]
+        inputs = np.random.random(np.product(input_shape)).reshape(input_shape)
+        output = self.host.post(URI.append("check_dnn_layer"), {"inputs": load_dense(inputs)})
+        self.assertEqual(output_shape(output), [BATCH_SIZE, 1])
 
 
 def load_dense(nparray):
     return tc.tensor.Dense.load(nparray.shape, tc.F32, nparray.flatten().tolist())
+
+
+def output_shape(as_json):
+    return as_json[tc.uri(tc.tensor.Dense)][0][0]
 
 
 if __name__ == "__main__":
