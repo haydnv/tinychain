ada9a8ba6249df3daf728a3cf57af97ff43eb3ac
Haydn Vestal <haydnv@gmail.com>
2022-03-24 17:33:39 +0000

    add tests for tc.app.App & allow Model classes as type annotations (#126)

--- client/tinychain/__init__.py
+++ client/tinychain/__init__.py
@@ -20,7 +20,7 @@ from tinychain.reflect import Meta
 from tinychain.generic import Map, Tuple
 from tinychain.interface import Interface, Equality, Order
 from tinychain.scalar.ref import After, Case, If, While  # TODO: replace with helper methods
-from tinychain.scalar.value import Id, String, Value
+from tinychain.scalar.value import EmailAddress, Id, String, Value
 from tinychain.scalar.number import Number, Bool, Complex, C32, C64, Float, F32, F64, Int, I16, I32, I64, UInt, U8, U16, U32, U64
 from tinychain.state import Class, Instance, State, Stream
 from tinychain.util import form_of, print_json, to_json, uri, Context, URI
