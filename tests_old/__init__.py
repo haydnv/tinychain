4cbc4c7a37ee61b026699f7b321e7c5402889748
haydnv <haydnv@gmail.com>
2022-04-18 12:40:32 +0200

    remove old tests directory

--- tests_old/__init__.py
+++
@@ -1,1 +0,0 @@
-# TODO: configure this as a package and consolidate all the testutils modules into one
--- tests_old/client/testutils.py
+++
@@ -1,9 +0,0 @@
-import os
-import tinychain as tc
-import unittest
-
-
-class ClientTest(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.host = tc.host.Host(os.getenv("TC_HOST", "http://demo.tinychain.net"))
