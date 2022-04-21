7426c5bfdf45436a16ad6c4396d04222abea8783
haydnv <haydnv@gmail.com>
2022-04-20 22:15:23 -0400

    create module for activation functions

--- client/tinychain/ml/__init__.py
+++ client/tinychain/ml/__init__.py
@@ -1,5 +1,6 @@
 """Machine learning utilities such as a standard definition of a :class:`NeuralNet` and :class:`Optimizer`"""
 
-from .constants import LIB_URI, sigmoid
+from .activation import sigmoid, softmax, relu
+from .constants import LIB_URI
 from .service import ML
 from .variable import Variable
---
+++ client/tinychain/ml/activation.py
@@ -0,0 +1,25 @@
+from ..collection.tensor import where, Tensor
+from ..math.operator import derivative_of, Unary
+
+
+def sigmoid(x):
+    return 1 / (1 + (-x).exp())
+
+
+def softmax(x, axis=0):
+    p = x.exp()
+    return p / p.sum(axis)
+
+
+# TODO: should there be a general "Where" operator?
+class ReLU(Unary):
+    def forward(self):
+        return where(self.subject > 0, self.subject, 0.)
+
+    def backward(self, variable):
+        d = derivative_of(self.subject, variable)
+        return where(d > 0, d, 0.)
+
+
+def relu(x):
+    return Tensor(ReLU(x))
