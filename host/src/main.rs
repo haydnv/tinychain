45f929ae1d089fcb1611e41f885a1a6c281c43c2
haydnv <haydnv@gmail.com>
2022-04-24 12:35:11 -0500

    allow overriding the per-thread stack size

--- host/src/main.rs
+++ host/src/main.rs
@@ -20,14 +20,14 @@ type TokioError = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 const MIN_CACHE_SIZE: usize = 5000;
 
-fn data_size(flag: &str) -> TCResult<u64> {
+fn data_size(flag: &str) -> TCResult<usize> {
     const ERR: &str = "unable to parse data size";
 
     if flag.is_empty() || flag == "0" {
         return Ok(0);
     }
 
-    let size = u64::from_str_radix(&flag[0..flag.len() - 1], 10)
+    let size = usize::from_str_radix(&flag[0..flag.len() - 1], 10)
         .map_err(|_| TCError::bad_request(ERR, flag))?;
 
     if flag.ends_with('K') {
@@ -51,23 +51,40 @@ fn duration(flag: &str) -> TCResult<Duration> {
 struct Config {
     #[structopt(
         long = "address",
-        default_value = "0.0.0.0",
-        about = "The IP address to bind"
+        about = "the IP address to bind",
+        default_value = "0.0.0.0"
     )]
     pub address: IpAddr,
 
-    #[structopt(long = "log_level", default_value = "warn")]
+    #[structopt(
+        long = "log_level",
+        about = "the log message level to write (options are trace, debug, warn, or error)",
+        default_value = "warn"
+    )]
     pub log_level: String,
 
     #[structopt(
         long = "workspace",
-        default_value = "/tmp/tc/tmp",
-        about = "workspace directory"
+        about = "the directory to use as a temporary workspace in case of a cache overflow",
+        default_value = "/tmp/tc/tmp"
     )]
     pub workspace: PathBuf,
 
-    #[structopt(long = "cache_size", default_value = "1G", parse(try_from_str = data_size))]
-    pub cache_size: u64,
+    #[structopt(
+        long = "cache_size",
+        about="the maximum size of the in-memory transactional filesystem cache (in bytes)",
+        default_value = "1G",
+        parse(try_from_str = data_size),
+    )]
+    pub cache_size: usize,
+
+    #[structopt(
+        long = "stack_size",
+        about="the size of the stack of each worker thread (in bytes)",
+        default_value = "4M",
+        parse(try_from_str = data_size),
+    )]
+    pub stack_size: usize,
 
     #[structopt(
         long = "data_dir",
@@ -75,18 +92,25 @@ struct Config {
     )]
     pub data_dir: Option<PathBuf>,
 
-    #[structopt(long = "cluster", about = "path(s) to Cluster config files")]
+    #[structopt(
+        long = "cluster",
+        about = "path(s) to cluster configuration files (this flag can be repeated)"
+    )]
     pub clusters: Vec<PathBuf>,
 
     #[structopt(
         long = "request_ttl",
+        about = "maximum allowed request duration",
         default_value = "30",
         parse(try_from_str = duration),
-        about = "maximum allowed request duration"
     )]
     pub request_ttl: Duration,
 
-    #[structopt(long = "http_port", default_value = "8702")]
+    #[structopt(
+        long = "http_port",
+        about = "the port that the HTTP server should listen on",
+        default_value = "8702"
+    )]
     pub http_port: u16,
 }
 
@@ -100,9 +124,7 @@ impl Config {
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), TokioError> {
-    let config = Config::from_args();
+async fn load_and_serve(config: Config) -> Result<(), TokioError> {
     let gateway_config = config.gateway();
 
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(config.log_level))
@@ -189,6 +211,27 @@ async fn main() -> Result<(), TokioError> {
     let kernel = tinychain::Kernel::new(clusters);
     let gateway = tinychain::gateway::Gateway::new(gateway_config, kernel, txn_server);
 
-    log::info!("starting server, cache size is {}", config.cache_size);
+    log::info!(
+        "starting server, stack size is {}, cache size is {}",
+        config.stack_size,
+        config.cache_size
+    );
+
     gateway.listen().await
+}
+
+fn main() {
+    let config = Config::from_args();
+
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_io()
+        .enable_time()
+        .thread_stack_size(config.stack_size)
+        .build()
+        .expect("tokio runtime");
+
+    match runtime.block_on(load_and_serve(config)) {
+        Ok(_) => {}
+        Err(cause) => panic!("HTTP server failed: {}", cause),
+    }
 }
