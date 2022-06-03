65017289984747804efc60e790a3b853aa5f4176
Cormac Murphy <cormacsmurf@gmail.com>
2022-06-03 23:40:46 +0100

    33 - Initial commit for graph schema creation.

--- tests/tctest/client/__init__.py
+++ tests/tctest/client/__init__.py
@@ -1,5 +1,6 @@
 from .test_linalg import LinearAlgebraTests
 from .test_operators import OperatorTests
 from .test_app import ModelTests
+from .test_graph import GraphTests
 from .test_table import TableTests
 from .test_tensor import TensorTests
--- tests/tctest/client/__main__.py
+++ tests/tctest/client/__main__.py
@@ -2,12 +2,14 @@ import logging
 import sys
 import unittest
 
-from .test_linalg import *
-from .test_operators import *
-from .test_table import *
-from .test_tensor import *
-from .test_app import *
+# from .test_linalg import *
+# from .test_operators import *
+# from .test_table import *
+# from .test_tensor import *
+# from .test_app import *
+from .test_graph import *
 
-logging.basicConfig(stream=sys.stderr)
+logging.getLogger("test_graph")
+logging.basicConfig(level=logging.DEBUG, stream=sys.stderr)
 
 unittest.main()
