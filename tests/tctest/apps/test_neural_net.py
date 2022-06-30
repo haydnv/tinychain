faccafad984bcf2e49d3f8d9974094a882f847fe
haydnv <haydnv@gmail.com>
2022-06-30 16:32:12 -0700

    add test for the derivative of a Sequential model

--- tests/tctest/apps/test_neural_net.py
+++ tests/tctest/apps/test_neural_net.py
@@ -35,6 +35,19 @@ class NeuralNetTester(tc.app.Library):
         layer2 = tc.ml.nn.Linear.create(2, 1)
         return tc.ml.nn.Sequential([layer1, layer2]).eval(inputs)
 
+    @tc.post
+    def test_sequential_derivative(self, cxt, inputs: tc.tensor.Tensor) -> tc.F32:
+        cxt.layer1 = tc.ml.nn.Linear.create(2, 3)
+        cxt.layer2 = tc.ml.nn.Linear.create(3, 2)
+        cxt.layer3 = tc.ml.nn.Linear.create(2, 1)
+        cxt.outputs = tc.ml.nn.Sequential([cxt.layer1, cxt.layer2, cxt.layer3]).eval(inputs)
+        vars = [cxt.layer1.weights, cxt.layer1.bias, cxt.layer2.weights, cxt.layer2.bias, cxt.layer3.weights, cxt.layer3.bias]
+        grads = tc.math.gradients(cxt.outputs, tc.tensor.Dense.ones_like(cxt.outputs), vars)
+        cxt.grads = grads
+        assert len(vars) == len(cxt.grads)
+        return [grad.shape for grad in cxt.grads]
+        # return [var.update(grad.sum(0)) for (var, grad) in zip(vars, grads)]
+
 
 class NeuralNetTests(unittest.TestCase):
     @classmethod
@@ -56,6 +69,10 @@ class NeuralNetTests(unittest.TestCase):
     def testSequential(self):
         inputs = np.random.random(2 * BATCH_SIZE).reshape([BATCH_SIZE, 2])
         self.host.post(URI.append("test_sequential"), {"inputs": load_dense(inputs)})
+
+    def testSequentialDerivative(self):
+        inputs = np.random.random(2 * BATCH_SIZE).reshape([BATCH_SIZE, 2])
+        grads = self.host.post(URI.append("test_sequential_derivative"), {"inputs": load_dense(inputs)})
 
     @classmethod
     def tearDownClass(cls) -> None:
