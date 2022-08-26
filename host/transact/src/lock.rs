9f9ff065bef889ba804468b36a4e3f82c2816205
haydnv <haydnv@gmail.com>
2022-08-26 17:02:52 -0500

    optimize TxnLock write performance

--- host/transact/src/lock.rs
+++ host/transact/src/lock.rs
@@ -73,13 +73,13 @@ impl<T> Drop for TxnLockReadGuard<T> {
     }
 }
 
-pub struct TxnLockWriteGuard<T> {
+pub struct TxnLockWriteGuard<T: Clone + PartialEq> {
     lock: TxnLock<T>,
     txn_id: TxnId,
     guard: OwnedRwLockWriteGuard<T>,
 }
 
-impl<T> TxnLockWriteGuard<T> {
+impl<T: Clone + PartialEq> TxnLockWriteGuard<T> {
     fn new(lock: TxnLock<T>, txn_id: TxnId, guard: OwnedRwLockWriteGuard<T>) -> Self {
         Self {
             lock,
@@ -89,7 +89,7 @@ impl<T> TxnLockWriteGuard<T> {
     }
 }
 
-impl<T> Deref for TxnLockWriteGuard<T> {
+impl<T: Clone + PartialEq> Deref for TxnLockWriteGuard<T> {
     type Target = T;
 
     fn deref(&self) -> &Self::Target {
@@ -97,21 +97,13 @@ impl<T> Deref for TxnLockWriteGuard<T> {
     }
 }
 
-impl<T> DerefMut for TxnLockWriteGuard<T> {
+impl<T: Clone + PartialEq> DerefMut for TxnLockWriteGuard<T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        let mut state = self
-            .lock
-            .inner
-            .state
-            .lock()
-            .expect("TxnLockWriteGuard::deref");
-        state.pending_writes.insert(self.txn_id);
-
         self.guard.deref_mut()
     }
 }
 
-impl<T> Drop for TxnLockWriteGuard<T> {
+impl<T: Clone + PartialEq> Drop for TxnLockWriteGuard<T> {
     fn drop(&mut self) {
         trace!("TxnLockWriteGuard::drop {}", self.lock.inner.name);
 
@@ -126,7 +118,12 @@ impl<T> Drop for TxnLockWriteGuard<T> {
             assert_eq!(readers, &0);
         }
 
+        if &lock_state.versions.canon == self.guard.deref() {
+            lock_state.pending_writes.remove(&self.txn_id);
+        }
+
         lock_state.writer = None;
+
         self.lock.wake();
     }
 }
@@ -136,7 +133,29 @@ struct Versions<T> {
     versions: HashMap<TxnId, Arc<RwLock<T>>>,
 }
 
-impl<T: Clone> Versions<T> {
+impl<T: Clone + PartialEq<T>> Versions<T> {
+    fn commit(&mut self, txn_id: &TxnId) -> bool {
+        if let Some(version) = self.versions.get(txn_id) {
+            let version = version.try_read().expect("transaction version");
+            if version.deref() == &self.canon {
+                // no-op
+                false
+            } else {
+                self.canon = version.clone();
+                true
+            }
+        } else {
+            // it's still valid to read the version at this transaction, so keep a copy around
+            let canon = self.canon.clone();
+            self.versions.insert(*txn_id, Arc::new(RwLock::new(canon)));
+            false
+        }
+    }
+
+    fn finalize(&mut self, txn_id: &TxnId) {
+        self.versions.remove(txn_id);
+    }
+
     fn get(self: &mut Versions<T>, txn_id: TxnId) -> Arc<RwLock<T>> {
         if let Some(version) = self.versions.get(&txn_id) {
             version.clone()
@@ -160,7 +179,7 @@ struct LockState<T> {
 impl<T> LockState<T> {
     fn try_read(&mut self, txn_id: &TxnId) -> TCResult<Option<&mut Versions<T>>> {
         for reserved in self.pending_writes.iter().rev() {
-            assert!(reserved <= &txn_id);
+            debug_assert!(reserved <= &txn_id);
 
             if reserved > &self.last_commit && reserved < &txn_id {
                 // if there's a pending write that can change the value at this txn_id, wait it out
@@ -199,6 +218,28 @@ impl<T> LockState<T> {
             )));
         }
 
+        if let Some(reader) = self.readers.keys().max() {
+            if reader > &txn_id {
+                // can't write-lock the past
+                return Err(TCError::conflict(format!(
+                    "can't acquire write lock at {} since it already has a read lock at {}",
+                    txn_id, reader
+                )));
+            }
+        }
+
+        if let Some(writer) = &self.writer {
+            // if there's an active write lock, wait it out
+            debug!("TxnLock has an active write lock at {}", writer);
+            return Ok(None);
+        } else if let Some(readers) = self.readers.get(&txn_id) {
+            // if there's an active read lock for this txn_id, wait it out
+            if readers > &0 {
+                debug!("TxnLock has {} active readers at {}", readers, txn_id);
+                return Ok(None);
+            }
+        }
+
         for pending in self.pending_writes.iter().rev() {
             if pending > &txn_id {
                 // can't write-lock the past
@@ -212,35 +253,13 @@ impl<T> LockState<T> {
                     "can't acquire write lock due to a pending write at {}",
                     pending
                 );
+
                 return Ok(None);
             }
-        }
-
-        if let Some(reader) = self.readers.keys().max() {
-            if reader > &txn_id {
-                // can't write-lock the past
-                return Err(TCError::conflict(format!(
-                    "can't acquire write lock at {} since it already has a read lock at {}",
-                    txn_id, reader
-                )));
-            }
-        }
-
-        if let Some(readers) = self.readers.get(&txn_id) {
-            // if there's an active read lock for this txn_id, wait it out
-            if readers > &0 {
-                debug!("TxnLock has {} active readers at {}", readers, txn_id);
-                return Ok(None);
-            }
-        }
-
-        if let Some(writer) = &self.writer {
-            // if there's an active write lock, wait it out
-            debug!("TxnLock has an active write lock at {}", writer);
-            return Ok(None);
         }
 
         self.writer = Some(*txn_id);
+        self.pending_writes.insert(*txn_id);
 
         Ok(Some(&mut self.versions))
     }
@@ -248,27 +267,15 @@ impl<T> LockState<T> {
 
 impl<T: Clone + Eq> LockState<T> {
     fn commit(&mut self, txn_id: &TxnId) {
-        if let Some(version) = self.versions.versions.get(txn_id) {
-            let version = version.try_read().expect("transaction version");
-            if version.deref() == &self.versions.canon {
-                // no-op
-            } else {
-                self.last_commit = *txn_id;
-                self.versions.canon = version.clone();
-            }
-        } else {
-            // it's still valid to read the version at this transaction, so keep a copy around
-            let canon = self.versions.canon.clone();
-            self.versions
-                .versions
-                .insert(*txn_id, Arc::new(RwLock::new(canon)));
-        };
+        if self.versions.commit(txn_id) {
+            self.last_commit = *txn_id;
+        }
 
         self.pending_writes.remove(txn_id);
     }
 
     fn finalize(&mut self, txn_id: &TxnId) {
-        self.versions.versions.remove(txn_id);
+        self.versions.finalize(txn_id);
         self.pending_writes.remove(txn_id);
     }
 }
@@ -319,7 +326,7 @@ impl<T> TxnLock<T> {
     }
 }
 
-impl<T: Clone> TxnLock<T> {
+impl<T: Clone + PartialEq> TxnLock<T> {
     /// Try to acquire a read lock.
     pub async fn read(&self, txn_id: TxnId) -> TCResult<TxnLockReadGuard<T>> {
         debug!("read {} at {}", self.inner.name, txn_id);
