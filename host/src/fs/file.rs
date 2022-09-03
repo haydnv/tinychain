a019ba403dca7add0f9f92e5690ac29356d6eba2
haydnv <haydnv@gmail.com>
2022-09-03 15:58:18 -0500

    reimplement File::new and File::load

--- host/src/fs/file.rs
+++ host/src/fs/file.rs
@@ -8,17 +8,19 @@ use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use futures::TryFutureExt;
+use log::{debug, trace};
 use safecast::AsType;
-use tokio::sync::broadcast::Sender;
-use tokio::sync::{Mutex, OwnedRwLockReadGuard, OwnedRwLockWriteGuard, RwLock};
+use tokio::sync::{OwnedRwLockReadGuard, OwnedRwLockWriteGuard, RwLock};
 
 use tc_error::*;
 use tc_transact::fs::{BlockData, BlockId, FileRead, FileWrite, Store};
+use tc_transact::lock::{TxnLock, TxnLockReadGuard, TxnLockWriteGuard};
 use tc_transact::{Transact, TxnId};
 
-use super::CacheBlock;
+use super::{io_err, CacheBlock, VERSION};
 
-type Listing = HashMap<BlockId, TxnId>;
+type Listing = HashSet<BlockId>;
 pub type FileReadGuard<B> = FileGuard<B, OwnedRwLockReadGuard<Listing>>;
 pub type FileWriteGuard<B> = FileGuard<B, OwnedRwLockWriteGuard<Listing>>;
 
@@ -26,6 +28,7 @@ struct Wake;
 
 pub struct BlockReadGuard<B> {
     cache: freqfs::FileReadGuard<CacheBlock, B>,
+    modified: TxnLockReadGuard<TxnId>,
 }
 
 impl<B> Deref for BlockReadGuard<B> {
@@ -38,6 +41,7 @@ impl<B> Deref for BlockReadGuard<B> {
 
 pub struct BlockWriteGuard<B> {
     cache: freqfs::FileWriteGuard<CacheBlock, B>,
+    modified: TxnLockWriteGuard<TxnId>,
 }
 
 impl<B> Deref for BlockWriteGuard<B> {
@@ -52,11 +56,6 @@ impl<B> DerefMut for BlockWriteGuard<B> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.cache.deref_mut()
     }
-}
-
-pub struct FileVersion<B> {
-    file: File<B>,
-    listing: RwLock<HashMap<BlockId, TxnId>>,
 }
 
 pub struct FileGuard<B, L> {
@@ -128,16 +127,12 @@ where
     }
 }
 
-struct FileState<B> {
-    cache: freqfs::DirLock<CacheBlock>,
-    canon: Listing,
-    versions: HashMap<TxnId, FileVersion<B>>,
-    tx: Sender<Wake>,
-}
-
 #[derive(Clone)]
 pub struct File<B> {
-    state: Arc<Mutex<FileState<B>>>,
+    canon: freqfs::DirLock<CacheBlock>,
+    versions: freqfs::DirLock<CacheBlock>,
+    listing: TxnLock<Listing>,
+    modified: Arc<RwLock<HashMap<BlockId, TxnLock<TxnId>>>>,
     phantom: PhantomData<B>,
 }
 
@@ -145,12 +140,103 @@ impl<B: BlockData> File<B>
 where
     CacheBlock: AsType<B>,
 {
+    #[cfg(debug_assertions)]
+    fn lock_name(fs_dir: &freqfs::Dir<CacheBlock>) -> String {
+        format!("block list of file {:?}", &*fs_dir)
+    }
+
+    #[cfg(not(debug_assertions))]
+    fn lock_name(fs_dir: &freqfs::Dir<CacheBlock>) -> String {
+        "block list of transactional file"
+    }
+
     pub fn new(canon: freqfs::DirLock<CacheBlock>) -> TCResult<Self> {
-        todo!()
+        let mut fs_dir = canon
+            .try_write()
+            .map_err(|cause| TCError::internal(format!("new file is already in use: {}", cause)))?;
+
+        if fs_dir.len() > 0 {
+            return Err(TCError::internal("new file is not empty"));
+        }
+
+        Ok(Self {
+            canon,
+            listing: TxnLock::new(Self::lock_name(&fs_dir), Listing::new()),
+            modified: Arc::new(Default::default()),
+            versions: fs_dir.create_dir(VERSION.to_string()).map_err(io_err)?,
+            phantom: PhantomData,
+        })
     }
 
     pub(super) async fn load(canon: freqfs::DirLock<CacheBlock>, txn_id: TxnId) -> TCResult<Self> {
-        todo!()
+        let mut fs_dir = canon.write().await;
+        let versions = fs_dir
+            .get_or_create_dir(VERSION.to_string())
+            .map_err(io_err)?;
+
+        let mut blocks = HashMap::new();
+        let mut present = HashSet::new();
+        let mut version = versions
+            .write()
+            .await
+            .create_dir(txn_id.to_string())
+            .map_err(io_err)?
+            .write()
+            .await;
+
+        for (name, block) in fs_dir.iter() {
+            if name.starts_with('.') {
+                debug!("File::load skipping hidden filesystem entry");
+                continue;
+            }
+
+            if name.len() < B::ext().len() + 1 || !name.ends_with(B::ext()) {
+                return Err(TCError::internal(format!(
+                    "block has invalid extension: {}",
+                    name
+                )));
+            }
+
+            let (size_hint, contents) = match block {
+                freqfs::DirEntry::File(block) => {
+                    let size_hint = block.size_hint().await;
+                    let contents = block
+                        .read()
+                        .map_ok(|contents| B::clone(&*contents))
+                        .map_err(io_err)
+                        .await?;
+
+                    (size_hint, contents)
+                }
+                freqfs::DirEntry::Dir(_) => {
+                    return Err(TCError::internal(format!(
+                        "expected block file but found directory: {}",
+                        name
+                    )))
+                }
+            };
+
+            let block_id: BlockId = name[..(name.len() - B::ext().len() - 1)]
+                .parse()
+                .map_err(TCError::internal)?;
+
+            present.insert(block_id.clone());
+
+            let lock_name = format!("block {}", block_id);
+            blocks.insert(block_id, TxnLock::new(lock_name, txn_id));
+
+            version
+                .create_file(name.clone(), contents, size_hint)
+                .map_err(io_err)?;
+        }
+
+        Ok(Self {
+            canon,
+            versions,
+            listing: TxnLock::new(Self::lock_name(&fs_dir), present),
+            modified: Arc::new(RwLock::new(blocks)),
+            phantom: Default::default(),
+        })
     }
 }
 
