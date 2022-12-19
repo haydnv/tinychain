658dba5d683333d42cefa7c05e422ec359117deb
haydnv <haydnv@gmail.com>
2022-12-19 18:27:11 +0530

    remove old InheritanceTests

--- tests/tctest/apps/__init__.py
+++ tests/tctest/apps/__init__.py
@@ -1,6 +1,5 @@
 from .test_app import UserTests
 from .test_btree import BTreeChainTests
-from .test_inheritance import InheritanceTests
 from .test_neural_net import NeuralNetTests
 from .test_optimizer import OptimizerTests
 from .test_table import TableChainTests
