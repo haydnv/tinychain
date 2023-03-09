774f8f868c376a756a708cd04daa3e5a04971170
Haydn Vestal <haydnv@gmail.com>
2023-03-09 12:09:32 +0530

    replace transact::fs with txfs

--- host/transact/src/lib.rs
+++ host/transact/src/lib.rs
@@ -5,6 +5,7 @@
 use async_hash::Output;
 use async_trait::async_trait;
 use destream::en;
+use freqfs::DirLock;
 use sha2::Sha256; // TODO: should this be exported by the async_hash crate?
 
 use tc_error::*;
@@ -12,27 +13,77 @@ use tcgeneric::Id;
 
 mod id;
 
-pub mod fs;
+pub mod fs {
+    use async_trait::async_trait;
+    use freqfs::FileLoad;
+
+    use super::{TCResult, Transaction, TxnId};
+
+    /// A transactional directory
+    pub type Dir<FE> = txfs::Dir<TxnId, FE>;
+
+    /// Defines how to load a persistent data structure from the filesystem.
+    #[async_trait]
+    pub trait Persist<FE: FileLoad>: Sized {
+        type Txn: Transaction<FE>;
+        type Schema: Clone + Send + Sync;
+
+        /// Create a new instance of [`Self`] from an empty `Store`.
+        async fn create(txn_id: TxnId, schema: Self::Schema, store: Dir<FE>) -> TCResult<Self>;
+
+        /// Load a saved instance of [`Self`] from persistent storage.
+        /// Should only be invoked at startup time.
+        async fn load(txn_id: TxnId, schema: Self::Schema, store: Dir<FE>) -> TCResult<Self>;
+
+        /// Load a saved instance of [`Self`] from persistent storage if present, or create a new one.
+        async fn load_or_create(
+            txn_id: TxnId,
+            schema: Self::Schema,
+            store: Dir<FE>,
+        ) -> TCResult<Self> {
+            if store.is_empty(txn_id).await? {
+                Self::create(txn_id, schema, store).await
+            } else {
+                Self::load(txn_id, schema, store).await
+            }
+        }
+
+        /// Access the filesystem directory backing this persistent data structure.
+        fn dir(&self) -> &freqfs::DirLock<FE>;
+    }
+
+    /// Copy a base state from another instance, possibly a view.
+    #[async_trait]
+    pub trait CopyFrom<FE: FileLoad, I>: Persist<FE> {
+        /// Copy a new instance of `Self` from an existing instance.
+        async fn copy_from(
+            txn: &<Self as Persist<FE>>::Txn,
+            store: Dir<FE>,
+            instance: I,
+        ) -> TCResult<Self>;
+    }
+
+    /// Restore a persistent state from a backup.
+    #[async_trait]
+    pub trait Restore<FE: FileLoad>: Persist<FE> {
+        /// Restore this persistent state from a backup.
+        async fn restore(&self, txn_id: TxnId, backup: &Self) -> TCResult<()>;
+    }
+}
 
 pub mod lock {
     use super::TxnId;
 
-    pub use txn_lock::{
-        Error as TxnLockError, TxnLockFinalize, TxnLockReadGuard, TxnLockReadGuardExclusive,
-        TxnLockRollback, TxnLockWriteGuard,
-    };
-
-    pub type TxnLock<T> = txn_lock::TxnLock<TxnId, T>;
-    pub type TxnLockCommit<T> = txn_lock::TxnLockCommit<TxnId, T>;
+    pub type TxnLock<T> = txn_lock::scalar::TxnLock<TxnId, T>;
 }
 
 pub use id::{TxnId, MIN_ID};
 
 /// Defines a method to compute the hash of this state as of a given [`TxnId`]
 #[async_trait]
-pub trait AsyncHash<D: fs::Dir> {
+pub trait AsyncHash<FE> {
     /// The type of [`Transaction`] which this state supports
-    type Txn: Transaction<D>;
+    type Txn: Transaction<FE>;
 
     /// Compute the hash of this state as of a given [`TxnId`]
     async fn hash(self, txn: &Self::Txn) -> TCResult<Output<Sha256>>;
@@ -40,9 +91,9 @@ pub trait AsyncHash<D: fs::Dir> {
 
 /// Access a view which can be encoded with [`en::IntoStream`].
 #[async_trait]
-pub trait IntoView<'en, D: fs::Dir> {
+pub trait IntoView<'en, FE> {
     /// The type of [`Transaction`] which this state supports
-    type Txn: Transaction<D>;
+    type Txn: Transaction<FE>;
 
     /// The type of encodable view returned by `into_view`
     type View: en::IntoStream<'en> + Sized + 'en;
@@ -69,16 +120,17 @@ pub trait Transact {
 
 /// Common transaction context properties.
 #[async_trait]
-pub trait Transaction<D: fs::Dir>: Clone + Sized + Send + Sync + 'static {
+pub trait Transaction<FE>: Clone + Sized + Send + Sync + 'static {
     /// The [`TxnId`] of this transaction context.
     fn id(&'_ self) -> &'_ TxnId;
 
-    /// Borrow the [`fs::Dir`] of this transaction context.
-    fn context(&'_ self) -> &'_ D;
+    /// Allows locking the filesystem directory of this transaction context,
+    /// e.g. to cache un-committed state or to compute an intermediate result.
+    fn context(&'_ self) -> &'_ DirLock<FE>;
 
-    /// Return a transaction subcontext with its own [`fs::Dir`].
+    /// Create a new transaction context with the given `id`.
     async fn subcontext(&self, id: Id) -> TCResult<Self>;
 
-    /// Return a transaction subcontext with its own unique [`fs::Dir`].
+    /// Create a new transaction subcontext with its own unique [`Dir`].
     async fn subcontext_unique(&self) -> TCResult<Self>;
 }
