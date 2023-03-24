a557ab1d637e9e96f43a574ea966b584d7606811
Haydn Vestal <haydnv@gmail.com>
2023-03-24 11:46:02 +0530

    bump sphinx & sphinx-rtd-theme required versions

--- tests/tctest/services/test_service.py
+++ tests/tctest/services/test_service.py
@@ -9,15 +9,15 @@ LEAD = "http://127.0.0.1:8702"
 NS = tc.URI("/test_service")
 
 
-class TestServiceV0(tc.service.Service):
+class TestService(tc.service.Service):
     NAME = "hello"
     VERSION = tc.Version("0.0.0")
 
     __uri__ = tc.service.service_uri(LEAD, NS, NAME, VERSION)
 
     @tc.get
-    def hello(self) -> tc.String:
-        return "Hello, World!"
+    def hello(self, name: tc.String) -> tc.String:
+        return tc.String("Hello, {{name}}!").render(name=name)
 
 
 class ServiceVersionTests(unittest.TestCase):
@@ -31,53 +31,25 @@ class ServiceVersionTests(unittest.TestCase):
 
         hosts[0].create_namespace(actor, tc.URI(tc.service.Service), NS, LEAD)
 
-        print()
-        print()
-
-        endpoint = tc.URI(TestServiceV0).path()[:-2]
-
-        for i in range(len(hosts)):
-            print()
-            print(f"host {i} replicas", hosts[i].get(endpoint.append("replicas")))
-            print()
-
-        print()
         hosts.append(start_host(NS, http_port=8704, public_key=actor.public_key, replicate=LEAD))
-        print()
 
-        for i in range(len(hosts)):
-            print()
-            print(f"host {i} replicas", hosts[i].get(endpoint.append("replicas")))
-            print()
+        hosts[0].install(actor, TestService())
 
-        hosts[0].install(actor, TestServiceV0())
-        print()
-
-        endpoint = tc.URI(TestServiceV0).path().append("hello")
+        endpoint = tc.URI(TestService).path().append("hello")
         for host in hosts:
-            print(host)
-            self.assertEqual(hosts[i].get(endpoint), "Hello, World!")
+            self.assertEqual(host.get(endpoint, "World"), "Hello, World!")
 
         hosts.append(start_host(NS, http_port=8705, public_key=actor.public_key, replicate=LEAD))
 
         for host in hosts:
-            print(host)
-            self.assertEqual(hosts[i].get(endpoint), "Hello, World!")
+            self.assertEqual(host.get(endpoint, "World"), "Hello, World!")
 
         hosts[2].stop()
 
-        print()
-        print("host stopped")
-        print()
-
         hosts[2].start()
 
-        print()
-        print("host started")
-        print()
-
         for host in hosts:
-            self.assertEqual(host.get(endpoint), "Hello, World!")
+            self.assertEqual(host.get(endpoint, "World"), "Hello, World!")
 
 
 def printlines(n):
