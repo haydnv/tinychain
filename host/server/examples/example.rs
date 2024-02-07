368ae7e3fbe41df4b6ef5406713a6c05e30c42a3
Haydn Vestal <haydnv@gmail.com>
2024-02-07 19:50:20 +0530

    stub out examples dir

---
+++ host/server/examples/example.rs
@@ -0,0 +1,28 @@
+// use cases to consider:
+//
+//  1.
+//  The first host in a cluster starts.
+//  It leaves a TCP server open on a random port for later hosts on the local network to discover,
+//  only if a shared symmetric key is configured.
+//
+//  2.
+//  A host stays running for months without restarting. It maintains an accurate list of peers.
+//
+//  3.
+//  A transaction causes a random, irregularly distributed set of 51% of all hosts
+//  in a cluster to fail.
+//
+//  4.
+//  A single cluster manages xx TBs of data spread across many shards. All the shards
+//  operated by one public cloud provider go offline simultaneously but users experience
+//  no interruption in service and no data is lost.
+//
+//  5.
+//  One host is running on a public cloud provider.
+//  A second host joins from an on-premises datacenter.
+//  The list of peers and replicas is updated such that both hosts receive every write operation.
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
