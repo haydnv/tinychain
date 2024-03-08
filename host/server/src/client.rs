9c4061856cfe1a1ea3203cec9b7e608e957cae64
Haydn Vestal <haydnv@gmail.com>
2024-03-08 12:06:46 +0530

    add indirection between Client and Kernel

--- host/server/src/client.rs
+++ host/server/src/client.rs
@@ -32,19 +32,104 @@ pub trait RPCClient: Send + Sync {
 }
 
 #[derive(Clone)]
+struct ClientInner {
+    port: u16,
+    kernel: Arc<Kernel>,
+    client: Arc<dyn RPCClient>,
+}
+
+impl ClientInner {
+    fn new(port: u16, kernel: Arc<Kernel>, client: Arc<dyn RPCClient>) -> Self {
+        Self {
+            port,
+            kernel,
+            client,
+        }
+    }
+
+    #[inline]
+    fn is_loopback(&self, link: &ToUrl) -> bool {
+        // todo: check if the port matches the default port for the protocol (e.g. 80 for HTTP)
+        link.host()
+            .map(|host| host.is_localhost() && host.port() == Some(self.port))
+            .unwrap_or(true)
+    }
+}
+
+#[async_trait]
+impl RPCClient for ClientInner {
+    async fn fetch(&self, txn_id: TxnId, link: ToUrl<'_>, actor_id: Value) -> TCResult<Actor> {
+        trace!("fetch actor {actor_id:?} at {link}");
+
+        if self.is_loopback(&link) {
+            let public_key = self
+                .kernel
+                .public_key(txn_id, link.path())
+                .map_err(rjwt::Error::fetch)?;
+
+            Ok(Actor::with_public_key(actor_id.clone(), public_key))
+        } else {
+            self.client.fetch(txn_id, link, actor_id).await
+        }
+    }
+
+    async fn get(&self, txn: &Txn, link: ToUrl<'_>, key: Value) -> TCResult<State> {
+        if self.is_loopback(&link) {
+            let endpoint = self.kernel.route(link.path(), txn)?;
+            let handler = endpoint.get(key)?;
+            handler.await
+        } else {
+            self.client.get(txn, link, key).await
+        }
+    }
+
+    async fn put(&self, txn: &Txn, link: ToUrl<'_>, key: Value, value: State) -> TCResult<()> {
+        if self.is_loopback(&link) {
+            let endpoint = self.kernel.route(link.path(), txn)?;
+            let handler = endpoint.put(key, value)?;
+            handler.await
+        } else {
+            self.client.put(txn, link, key, value).await
+        }
+    }
+
+    async fn post(&self, txn: &Txn, link: ToUrl<'_>, params: Map<State>) -> TCResult<State> {
+        if self.is_loopback(&link) {
+            let endpoint = self.kernel.route(link.path(), txn)?;
+            let handler = endpoint.post(params)?;
+            handler.await
+        } else {
+            self.client.post(txn, link, params).await
+        }
+    }
+
+    async fn delete(&self, txn: &Txn, link: ToUrl<'_>, key: Value) -> TCResult<()> {
+        if self.is_loopback(&link) {
+            let endpoint = self.kernel.route(link.path(), txn)?;
+            let handler = endpoint.delete(key)?;
+            handler.await
+        } else {
+            self.client.delete(txn, link, key).await
+        }
+    }
+}
+
+// use an additional struct to provide indirection when calling RPCClient methods
+// in order to minimize build time
+#[derive(Clone)]
 pub(crate) struct Client {
     host: Host,
-    kernel: Arc<Kernel>,
     client: Arc<dyn RPCClient>,
     egress: Option<Arc<dyn Egress>>,
 }
 
 impl Client {
     pub fn new(host: Host, kernel: Arc<Kernel>, client: Arc<dyn RPCClient>) -> Self {
+        let client = ClientInner::new(host.port().expect("port"), kernel, client);
+
         Self {
             host,
-            kernel,
-            client,
+            client: Arc::new(client),
             egress: None,
         }
     }
@@ -52,7 +137,6 @@ impl Client {
     pub fn with_egress(self, egress: Arc<dyn Egress>) -> Self {
         Self {
             host: self.host,
-            kernel: self.kernel,
             client: self.client,
             egress: Some(egress),
         }
@@ -75,77 +159,31 @@ impl Client {
             Err(unauthorized!("egress to {link}"))
         }
     }
-
-    #[inline]
-    fn is_loopback(&self, link: &ToUrl) -> bool {
-        link.host()
-            .map(|host| host.is_localhost() && host.port() == self.host.port())
-            .unwrap_or(true)
-    }
 }
 
 #[async_trait]
 impl RPCClient for Client {
     async fn fetch(&self, txn_id: TxnId, link: ToUrl<'_>, actor_id: Value) -> TCResult<Actor> {
-        trace!("fetch actor {actor_id:?} at {link}");
-
-        if self.is_loopback(&link) {
-            let public_key = self
-                .kernel
-                .public_key(txn_id, link.path())
-                .map_err(rjwt::Error::fetch)?;
-
-            Ok(Actor::with_public_key(actor_id.clone(), public_key))
-        } else {
-            self.client.fetch(txn_id, link, actor_id).await
-        }
+        self.client.fetch(txn_id, link, actor_id).await
     }
 
     async fn get(&self, txn: &Txn, link: ToUrl<'_>, key: Value) -> TCResult<State> {
         self.authorize(&link, false)?;
-
-        if self.is_loopback(&link) {
-            let endpoint = self.kernel.route(link.path(), txn)?;
-            let handler = endpoint.get(key)?;
-            handler.await
-        } else {
-            self.client.get(txn, link, key).await
-        }
+        self.client.get(txn, link, key).await
     }
 
     async fn put(&self, txn: &Txn, link: ToUrl<'_>, key: Value, value: State) -> TCResult<()> {
         self.authorize(&link, true)?;
-
-        if self.is_loopback(&link) {
-            let endpoint = self.kernel.route(link.path(), txn)?;
-            let handler = endpoint.put(key, value)?;
-            handler.await
-        } else {
-            self.client.put(txn, link, key, value).await
-        }
+        self.client.put(txn, link, key, value).await
     }
 
     async fn post(&self, txn: &Txn, link: ToUrl<'_>, params: Map<State>) -> TCResult<State> {
         self.authorize(&link, true)?;
-
-        if self.is_loopback(&link) {
-            let endpoint = self.kernel.route(link.path(), txn)?;
-            let handler = endpoint.post(params)?;
-            handler.await
-        } else {
-            self.client.post(txn, link, params).await
-        }
+        self.client.post(txn, link, params).await
     }
 
     async fn delete(&self, txn: &Txn, link: ToUrl<'_>, key: Value) -> TCResult<()> {
         self.authorize(&link, true)?;
-
-        if self.is_loopback(&link) {
-            let endpoint = self.kernel.route(link.path(), txn)?;
-            let handler = endpoint.delete(key)?;
-            handler.await
-        } else {
-            self.client.delete(txn, link, key).await
-        }
+        self.client.delete(txn, link, key).await
     }
 }
