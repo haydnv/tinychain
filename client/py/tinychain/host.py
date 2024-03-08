c34d71a9ec9825c31d634ba1c3f0236da1c9a515
Haydn Vestal <haydnv@gmail.com>
2024-03-08 15:19:02 +0530

    simplify the Host.install method

--- client/py/tinychain/host.py
+++ client/py/tinychain/host.py
@@ -1,19 +1,15 @@
 """Utilities for communicating with a TinyChain host."""
 
 import abc
-import inspect
-import logging
 
 import json
 import requests
-import rjwt
 import urllib.parse
 
 from .service import Library, Model, Service
 from .context import to_json
 from .error import *
 from .scalar.value import Nil
-from .state import State
 from .uri import URI
 
 
@@ -146,114 +142,33 @@ class Host(object):
 
         return self._handle(request)
 
+    def _create_namespace(self, path, is_dir):
+        assert path[:1] in [URI(Library), URI(Service)], f"invalid library or service prefix: {path[0]}"
+        self.put(URI(Model), path[-1], is_dir)
+        self.put(path[:-1], path[-1], is_dir)
+
+    def _create_namespace_all(self, path):
+        assert path
+
+        exists = 1
+        while exists < len(path):
+            try:
+                self.get(path[:exists + 1])
+                exists += 1
+            except NotFound:
+                break
+
+        for i in range(exists, len(path) - 1):
+            self._create_namespace(path[:i], True)
+
+        self._create_namespace(path, False)
+
+    def install(self, service):
+        self._create_namespace_all(URI(service)[:-1])
+        self.put(URI(service)[:-1], URI(service)[-1], service)
+
     def hypothetical(self, op_def, auth=None):
         return self.post("/transact/hypothetical", {"op": op_def}, auth)
-
-    def _namespace_args(self, actor, base_dir, ns, lead):
-        ns = URI(ns)
-        lead = None if lead is None else URI(lead)
-
-        if ns.host() is not None:
-            raise ValueError(f"namespace {ns} should not include a host")
-
-        if len(ns) != 1:
-            raise NotImplementedError(f"create a namespace {ns} (len {len(ns)}) by creating directories recursively")
-
-        if lead is not None and lead.path() is not None:
-            raise ValueError(f"lead for {ns} ({lead}) should not have a path component")
-
-        logging.info(f"create namespace {base_dir}{ns} on {self}")
-
-        auth_host = lead if lead else URI(self)
-
-        issuer = str(auth_host + base_dir)
-        token = rjwt.Token.issue(issuer, '/', [str(ns)])
-        token = actor.sign_token(token)
-
-        issuer = str(auth_host + URI(Model))
-        token = rjwt.Token.consume(token, issuer, '/', [str(ns)])
-        token = actor.sign_token(token)
-
-        lead = base_dir + ns if lead is None else lead + base_dir + ns
-        return base_dir, str(ns)[1:], lead, token
-
-    def create_namespace(self, actor, base_dir, ns, lead=None):
-        return self.put(*self._namespace_args(actor, base_dir, ns, lead))
-
-    def _install_args(self, actor, service):
-        if isinstance(service, Service):
-            if not URI(service).path().startswith(URI(Service)):
-                raise ValueError(f"invalid path for Service: {URI(service)}")
-        elif isinstance(service, Library):
-            if not URI(service).path().startswith(URI(Library)):
-                raise ValueError(f"invalid path for Library: {URI(service)}")
-        else:
-            raise ValueError(f"not a Library or Service: {service}")
-
-        lead = URI(service)[0] if URI(service).host() else None
-        path = URI(service).path()
-        base_dir = path[0]
-        ns = path[1:-2]
-        name = path[-2]
-        _version = path[-1]
-
-        logging.info(f"install {name} at {base_dir}{ns} on {self}")
-
-        if URI(service).host() is None:
-            lead = URI(self)
-        else:
-            lead = URI(service)[0]
-
-        deps = []
-        for _, cls in inspect.getmembers(service, inspect.isclass):
-            if issubclass(cls, Library):
-                deps.append(cls)
-            elif issubclass(cls, State):
-                continue
-            else:
-                logging.info(f"install {service} will skip dependency {cls} since it's not a Library or a State")
-
-        if deps:
-            logging.info(f"installing {service} which depends on {deps}...")
-        else:
-            logging.info(f"installing {service}")
-
-        auth_host = URI(lead) if lead else URI(self)
-        issuer = str(auth_host + base_dir + ns)
-        token = rjwt.Token.issue(issuer, '/', [str(name)])
-        token = actor.sign_token(token)
-
-        issuer = str(auth_host + URI(Model) + ns)
-        token = rjwt.Token.consume(token, issuer, '/', [str(name)])
-        token = actor.sign_token(token)
-
-        return URI(service).path()[:-2], str(name)[1:], service, token
-
-    def install(self, actor, service):
-        """Install the given `service` on this host"""
-
-        return self.put(*self._install_args(actor, service))
-
-    def update(self, actor, service):
-        """Update the version of given `service` on this host"""
-
-        lead = URI(service)[0] if URI(service).host() else None
-        path = URI(service).path()
-        base_dir = path[0]
-        ns = path[1:-2]
-        name = path[-2]
-        version = path[-1]
-
-        auth_host = URI(lead) if lead else URI(self)
-        issuer = str(auth_host + base_dir + ns)
-        token = rjwt.Token.issue(issuer, '/', [str(name + version)])
-        token = actor.sign_token(token)
-
-        issuer = str(auth_host + URI(Model) + ns)
-        token = rjwt.Token.consume(token, issuer, '/', [str(name)])
-        token = actor.sign_token(token)
-
-        return self.put(URI(service).path()[:-1], str(version)[1:], service, token)
 
 
 class Local(Host):
